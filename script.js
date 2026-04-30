812723417320ef7c760588f528bb780cce58ebee
pavendansg <pavendansg@gmail.com>
2026-04-30 23:01:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// script.js
-
 function unlockPremium(){
-alert(
-"🔥 PavenFitzz AI Premium\n\n" +
-"Price: ₹89 Lifetime Access\n\n" +
-"UPI Payment Integration Coming Next.\n\n" +
-"After payment users will unlock:\n" +
-"✔ AI Workout Plans\n" +
-"✔ AI Diet Plans\n" +
-"✔ Budget Plans\n" +
-"✔ Meal Prep"
-);
+window.location.href="ai-dashboard.html";
 }

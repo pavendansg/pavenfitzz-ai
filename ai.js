6e1195d738780598c24dfad4b5eea3808588d885
pavendansg <pavendansg@gmail.com>
2026-05-09 14:50:04 +0530

    Update ai.js

--- ai.js
+++ ai.js
@@ -3,7 +3,7 @@ const GEMINI_KEY = "AIzaSyBDkPioz9apRz7nb--AE00VEmUKcTgW-ak";
 
 async function callAI(prompt) {
   const res = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_KEY}`,
+    ``https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=${GEMINI_KEY}`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
